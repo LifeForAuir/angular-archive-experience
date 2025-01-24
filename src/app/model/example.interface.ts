d4aa1916f364823b08394f8f6b6336f201ea267b
Dudorov <dudorov@elma-bpm.com>
2025-01-24 16:00:01 +0400

    первый пример

--- src/app/model/example.interface.ts
+++ src/app/model/example.interface.ts
@@ -9,13 +9,50 @@ export interface Example {
 export const TypescriptExample: Example[] = [
     {
       id: 1,
-      title: 'Пример 1: Функция',
-      description: 'Простейшая функция приветствия.',
+      title: 'Условные типы (Conditional Types)',
+      description: `
+      Разберем код:
+        1.  type IsString<T> = ...:
+            *   Это объявление типа с именем IsString.
+            *   <T>:  Обозначает, что это generic-тип. T – это переменная типа (type variable), которая может принимать любой тип.
+            *   = ...:  Определяет значение типа.
+
+        2.  T extends string ? true : false:
+            *   Это условный тип (conditional type). Он работает как тернарный оператор в JavaScript (condition ? trueExpression : falseExpression), но в контексте типов.
+            *   T extends string:  Проверяет, является ли тип T подтипом (subtype) типа string.  Это не означает, что T *равен* string, а лишь, что T совместим с типом string (например, строковый литерал, как 'hello', будет extends string).
+            *   ? true : false:
+                *   Если T является подтипом string, то результатом типа IsString<T> будет true.
+                *   В противном случае (если T не является подтипом string) результатом будет false.|
+      `,
       code: `
-      function greet(name: string): string {
-        return "Hello, " + name + "!";
-      }
-      console.log(greet("World"));
+    // Определяем условный тип IsString
+    type IsString<T> = T extends string ? true : false;
+
+    // Примеры использования
+
+    // Тип A будет true, потому что string является строкой
+    type A = IsString<string>; // type A = true
+
+    // Тип B будет false, потому что number не является строкой
+    type B = IsString<number>; // type B = false
+
+    // Тип C будет true, потому что 'hello' - это строковый литерал
+    type C = IsString<'hello'>; // type C = true
+
+    // Тип D будет false, потому что boolean не является строкой
+    type D = IsString<boolean>; // type D = false
+
+    // Тип E будет false, потому что null не является строкой
+    type E = IsString<null>; // type E = false
+
+    // Тип F будет false, потому что undefined не является строкой
+    type F = IsString<undefined>; // type F = false
+
+    // Тип G будет false, потому что объект не является строкой
+    type G = IsString<{}>; // type G = false
+
+    // Тип H будет false, потому что массив не является строкой
+    type H = IsString<number[]>; // type H = false
       `,
       sourceUrl: 'https://habr.com/ru/articles/871336/',
     },
