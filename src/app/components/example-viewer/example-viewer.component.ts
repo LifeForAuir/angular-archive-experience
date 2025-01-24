7cdec9045ba438fcedc22bf1ef3c9594eb7c40a0
Dudorov <dudorov@elma-bpm.com>
2025-01-24 16:26:16 +0400

    Добавли обработку description

--- src/app/components/example-viewer/example-viewer.component.ts
+++ src/app/components/example-viewer/example-viewer.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, AfterViewInit } from '@angular/core';
+import { Component, Input, AfterViewInit, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-typescript';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import * as marked from 'marked';
 
 
 @Component({
@@ -11,14 +13,33 @@ import 'prismjs/components/prism-typescript';
   templateUrl: './example-viewer.component.html',
   styleUrls: ['./example-viewer.component.css']
 })
-export class ExampleViewerComponent implements AfterViewInit {
+export class ExampleViewerComponent implements AfterViewInit, OnInit, OnChanges {
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() code: string = '';
   @Input() sourceUrl: string = '';
 
-    ngAfterViewInit() {
+  protected safeDescription: SafeHtml | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  async ngOnInit() {
+    this.generateSafeDescription(this.description);
+ }
+
+  ngAfterViewInit() {
     Prism.highlightAll();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['description']) {
+        this.generateSafeDescription(changes['description'].previousValue);
+    }
+  }
+
+  private async generateSafeDescription(description: string) {
+    const html = await marked.parse(description);
+    this.safeDescription = this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
 
